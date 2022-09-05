9ef494deb32d586d9f7afa268d1e54ec30dcf4a7
l-Username-l <zhmyhun@gmail.com>
2022-09-05 10:08:00 +0900

    commit

---
+++ JavaMySQL_MiniProject-01.java
@@ -0,0 +1,9 @@
+public class Process_MySQL_Java_Mini_No_1 {
+    
+    public static void main(String[] args) {
+        // 1. 데이터베이스 받기
+        // 2. Data import
+        // 3. Check imported data
+        // 4. 무슨 과제인지 모르겠지만 과제 하기
+    }
+}
